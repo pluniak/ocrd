7f02f2ab72520e2c3501cde3abe887fd1b8f9249
Philipp Luniak <philipp.luniak@vigram.com>
2024-04-24 15:26:24 +0200

    line_segmentation; test set

--- create_conda_env.bat
+++ create_conda_env.bat
@@ -2,7 +2,7 @@
 REM Rune this file in Windows from a Conda terminal
 
 REM Name of the Conda environment
-set ENV_NAME=ocdr
+set ENV_NAME=ocrd
 
 REM Path to the requirements.txt file
 set REQUIREMENTS_FILE=requirements.txt
@@ -11,7 +11,7 @@ REM Check if the Conda environment exists
 conda info --envs | findstr /C:"%ENV_NAME%" > nul
 if errorlevel 1 (
     echo Environment %ENV_NAME% does not exist, creating...
-    conda create --name %ENV_NAME% python=3.8 --yes
+    conda create --name %ENV_NAME% python=3.10 --yes
     echo Activating environment and installing packages from requirements.txt using pip...
     call activate %ENV_NAME%
     pip install -r %REQUIREMENTS_FILE%
